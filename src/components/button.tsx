6723217bc8f5d15df8795c25b6ee8f48de9c6dca
Ashish Mehra <sdashish.am@gmail.com>
2021-05-28 18:52:56 +0530

    Modification

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,8 +1,40 @@
+import { useTheme } from "@shopify/restyle";
 import React from "react";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, StyleSheet, Text } from "react-native";
 
-const Button = () => {
-  return <TouchableOpacity></TouchableOpacity>;
+import { Theme } from "../theme";
+
+interface ButtonProps {
+  variant: "primary" | "secondary" | "transparent";
+  label: string;
+  onPress: () => void;
+}
+
+const Button = ({ variant, label, onPress }: ButtonProps) => {
+  const theme = useTheme<Theme>();
+  const backgroundColor =
+    variant === "primary" ? theme.colors.primary : variant === "transparent" ? "transparent" : theme.colors.secondary;
+
+  return (
+    <TouchableOpacity style={[styles.container, { backgroundColor }]}>
+      <Text>{label}</Text>
+    </TouchableOpacity>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 25,
+    height: 50,
+    width: 245,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+  },
+  label: {
+    fontSize: 15,
+    color: "#000000",
+  },
+});
 
 export default Button;
